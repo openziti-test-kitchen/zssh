2fdde945462a18af5f7e90cdb5c413ebe4a931ab
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2024-10-07 14:32:52 -0400

    log token only at debug

--- zsshlib/oidc.go
+++ zsshlib/oidc.go
@@ -149,7 +149,7 @@ func GetToken(ctx context.Context, config *OIDCConfig) (string, error) {
 
 	select {
 	case tokens := <-resultChan:
-		Logger().Infof("Refresh token: %s", tokens.RefreshToken)
+		Logger().Debugf("Refresh token: %s", tokens.RefreshToken)
 		return tokens.AccessToken, nil
 	case <-ctx.Done():
 		return "", errors.New("timeout: OIDC authentication took too long")
